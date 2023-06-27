6e00ad23adfd2c446f29244de8dcacea481f70b2
MrDuffs <chulkov.i@yandex.ru>
2023-06-27 22:48:00 +0300

    add logic for indicator

--- src/containers/target-indicator-content/index.jsx
+++ src/containers/target-indicator-content/index.jsx
@@ -1,13 +1,56 @@
-import { memo } from 'react';
+import {
+  memo, useEffect, useLayoutEffect, useState,
+} from 'react';
 import ContentLayout from '../../components/content-layout';
 import TargetIndicatorInfo from '../../components/target-indicator-info';
 import TargetIndicatorProgress from '../../components/target-indicator-progress';
+import { calculateProgress } from '../../utils/calculate-progress';
 
 function TargetIndicatorContent() {
+  const initialValue = 14;
+  const finalTarget = 15;
+  const [currentValue, setCurrentValue] = useState(0);
+  const [progress, setProgress] = useState(0);
+
+  useEffect(() => {
+    if (currentValue < initialValue) {
+      setTimeout(() => {
+        setCurrentValue((prev) => prev + 1);
+      }, 50);
+    }
+  }, [currentValue]);
+
+  useLayoutEffect(() => {
+    setProgress(() => calculateProgress(currentValue, finalTarget));
+  }, [currentValue]);
+
+  useEffect(() => {
+    if (currentValue >= initialValue && currentValue < finalTarget) {
+      setTimeout(() => {
+        setCurrentValue((prev) => +(prev + 0.2).toFixed(1));
+      }, 2000);
+    }
+  }, [currentValue]);
+
+  // const callbacks = {
+  //   onProgress: useCallback(() => {
+  //     progress < finalTarget ? setProgress((prev) => prev + 0.2) : setProgress(0);
+  //   }, [progress]),
+  // };
+
   return (
     <ContentLayout>
-      <TargetIndicatorProgress />
-      <TargetIndicatorInfo />
+      <TargetIndicatorProgress
+        currentTargetValue={currentValue}
+        progress={progress}
+        finalTarget={finalTarget}
+      />
+      {currentValue !== finalTarget && (
+        <TargetIndicatorInfo
+          currentTargetValue={currentValue}
+          finalTarget={finalTarget}
+        />
+      )}
     </ContentLayout>
   );
 }
