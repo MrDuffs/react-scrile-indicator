5b8e7bcc57874002df92a1b15656ed0e3ae69f90
MrDuffs <chulkov.i@yandex.ru>
2023-06-27 22:50:04 +0300

    add props for other components and edit styles

--- src/components/target-indicator-progress/index.jsx
+++ src/components/target-indicator-progress/index.jsx
@@ -1,12 +1,21 @@
 import { memo } from 'react';
 import ProgressBar from '../progress-bar';
+import TargetBlock from '../target-block';
 import './style.scss';
 
-function TargetIndicatorProgress(props) {
+function TargetIndicatorProgress({ currentTargetValue, progress, finalTarget }) {
   return (
     <div className="target-indicator-progress">
       <div className="target-indicator-progress__description">Reached:</div>
-      <ProgressBar />
+      <ProgressBar
+        currentTargetValue={currentTargetValue}
+        progress={progress}
+        finalTarget={finalTarget}
+      />
+      <TargetBlock
+        currentTargetValue={currentTargetValue}
+        finalTarget={finalTarget}
+      />
     </div>
   );
 }
