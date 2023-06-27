8980e713d7bd6ec9ee1f6a1ec52db44b0c19f909
MrDuffs <chulkov.i@yandex.ru>
2023-06-27 22:51:52 +0300

    add props and calculate value

--- src/components/target-indicator-info/index.jsx
+++ src/components/target-indicator-info/index.jsx
@@ -1,13 +1,14 @@
 import { memo } from 'react';
 import info from '../../assets/info.svg';
+import { getValueToBeReached } from '../../utils/get-value-to-be-reached';
 import './style.scss';
 
-function TargetIndicatorInfo(props) {
+function TargetIndicatorInfo({ currentTargetValue, finalTarget }) {
   return (
     <div className="target-indicator-info">
       <img src={info} className="target-indicator-info__img" alt="info" />
       <div className="target-indicator-info__text">
-        You need $1 more to reach your target.
+        You need ${getValueToBeReached(finalTarget, currentTargetValue)} more to reach your target.
       </div>
     </div>
   );
