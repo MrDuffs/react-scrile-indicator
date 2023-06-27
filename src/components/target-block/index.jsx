0d8b2580d81ed891e775d7db2e57922d6d0ca860
MrDuffs <chulkov.i@yandex.ru>
2023-06-27 22:52:24 +0300

    add props, logic for target block and edit styles

--- src/components/target-block/index.jsx
+++ src/components/target-block/index.jsx
@@ -1,13 +1,25 @@
 import { memo } from 'react';
 import './style.scss';
 
-function TargetBlock(props) {
+function TargetBlock({ currentTargetValue, finalTarget }) {
+  const onSuccessStyle = () => {
+    if (currentTargetValue === finalTarget) {
+      return {
+        background: '#00A910',
+      };
+    }
+    return null;
+  };
+
   return (
-    <div className="target-block">
+    <div
+      className="target-block"
+      style={onSuccessStyle()}
+    >
       <div className="target-block__title">Target</div>
       <div className="target-block__divider" />
       { /* <div className="target-block__divider_mask" /> */ }
-      <div className="target-block__goal">$15</div>
+      <div className="target-block__goal">${finalTarget}</div>
     </div>
   );
 }
