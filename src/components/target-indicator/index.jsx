963fa3e85be72d4460598cf795230f55ad69604a
MrDuffs <chulkov.i@yandex.ru>
2023-06-28 14:46:21 +0300

    load indicator values

--- src/components/target-indicator/index.jsx
+++ src/components/target-indicator/index.jsx
@@ -1,8 +1,16 @@
-import { memo } from 'react';
+import { memo, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import TargetIndicatorContent from '../../containers/target-indicator-content';
+import { loadIndicatorValues } from '../../store/reducers/target-indicator/actions';
 import './style.scss';
 
 function TargetIndicator() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(loadIndicatorValues());
+  }, [dispatch]);
+
   return (
     <div className="target-indicator">
       <div className="target-indicator__header">
