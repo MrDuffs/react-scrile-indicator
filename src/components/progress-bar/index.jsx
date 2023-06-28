582d67a8ee2294d284822da6fc8938f2fa858d36
MrDuffs <chulkov.i@yandex.ru>
2023-06-28 14:48:54 +0300

    add logic for dynamically change of tooltip width

--- src/components/progress-bar/index.jsx
+++ src/components/progress-bar/index.jsx
@@ -1,7 +1,35 @@
-import { memo } from 'react';
+import {
+  memo, useEffect, useRef, useState,
+} from 'react';
 import './style.scss';
 
 function ProgressBar({ currentTargetValue, progress, finalTarget }) {
+  const [tooltipWidth, setTooltipWidth] = useState(0);
+  const tooltipRef = useRef();
+
+  useEffect(() => {
+    if (tooltipRef.current) {
+      const tooltipStyle = tooltipRef.current.style.width;
+      if (currentTargetValue !== finalTarget) {
+        if (+tooltipStyle.slice(0, -1) < 38) setTooltipWidth(+tooltipStyle.slice(0, -1) + 1);
+      }
+    }
+  }, [currentTargetValue, finalTarget, tooltipWidth]);
+
+  const getTooltipStyles = () => {
+    if (currentTargetValue % 1 !== 0) {
+      return {
+        textAlign: 'start',
+        width: `${tooltipWidth}%`,
+        left: `${progress + 1.5}%`,
+      };
+    }
+    return {
+      width: '26%',
+      left: `${progress}%`,
+    };
+  };
+
   return (
     <div className="progress-bar">
       <div className="progress-bar__outer">
@@ -11,8 +39,9 @@ function ProgressBar({ currentTargetValue, progress, finalTarget }) {
         />
         {currentTargetValue !== finalTarget && (
           <span
+            ref={tooltipRef}
             className="progress-bar__tooltip"
-            style={{ left: `${progress}%` }}
+            style={getTooltipStyles()}
           >
             ${currentTargetValue}
           </span>
